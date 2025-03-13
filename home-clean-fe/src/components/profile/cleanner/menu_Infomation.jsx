3643f170d9a9e97631bd9a22e23d02ee2ae2aaca
NgynSon <sonnxhe163813@fpt.edu.vn>
2025-03-14 00:31:26 +0700

    force

--- home-clean-fe/src/components/profile/cleanner/menu_Infomation.jsx
+++ home-clean-fe/src/components/profile/cleanner/menu_Infomation.jsx
@@ -1,26 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { Link } from "react-router-dom";
 import infoImg from "../../../assets/imgProfile/info.svg";
 import addressImg from "../../../assets/imgProfile/address.svg";
 import discountImg from "../../../assets/imgProfile/discount.svg";
 import paymentImg from "../../../assets/imgProfile/payment.svg";
-import favoriteImg from "../../../assets/imgProfile/favorite.svg";
 import helpImg from "../../../assets/imgProfile/help.svg";
 import profileImg from "../../../assets/imgProfile/imgProfile.svg";
 import "../owner/profile.css";
 
 const MenuInfomation = ({ selectedMenu, setSelectedMenu }) => {
   const { cleaner } = useContext(AuthContext); // Lấy thông tin người dùng
+  const [cleanerImg, setImg] = useState(cleaner?.cleanerImg || "");
 
   const handleClick = (menuName) => {
     setSelectedMenu(menuName);
   };
-
+  useEffect(() => {
+    if (cleaner) {
+      if (cleaner.profile_image) {
+        setImg(`data:image/png;base64,${cleaner.profile_image}`);
+      } else {
+        setImg(profileImg); // Ảnh mặc định nếu không có ảnh từ API
+      }
+    } else {
+      setImg(profileImg);
+    }
+  }, [cleaner]);
   return (
     <div className="menu-wrapper">
       <div className="menu-profile">
-        <img className="profile-avatar" src={profileImg} alt="icon" />
+        <img className="profile-avatar" src={cleanerImg} alt="icon" />
         <div className="profile-details">
           <p className="profile-name"><strong>{cleaner?.cleanerName || "Người dùng"}</strong></p>
           <p className="profile-email">{cleaner?.cleanerEmail || "Chưa có email"}</p>
@@ -34,8 +44,7 @@ const MenuInfomation = ({ selectedMenu, setSelectedMenu }) => {
           { id: "2", label: "Địa chỉ", icon: addressImg },
           { id: "3", label: "Ưu đãi của tôi", icon: discountImg },
           { id: "4", label: "Quản lý thanh toán", icon: paymentImg },
-          { id: "5", label: "Nhân viên yêu thích", icon: favoriteImg },
-          { id: "6", label: "Trợ giúp", icon: helpImg },
+          { id: "5", label: "Trợ giúp", icon: helpImg },
         ].map((item) => (
           <div key={item.id} className="menu-item">
             <Link
